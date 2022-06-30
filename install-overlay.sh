8ff50f22a7d763fefdb5f4b652286de9e24de558
Ruediger zu Dohna <snackbox@sinntr.eu>
2022-06-30 14:03:42 +0200

    ignore WF minor version for overlay url

--- install-overlay.sh
+++ install-overlay.sh
@@ -8,7 +8,7 @@ WILDFLY_VERSION=$1
 WILDFLY_GRAPHQL_VERSION=$2
 
 # shellcheck disable=SC2001
-WILDFLY_GRAPHQL_BASE_VERSION=$(echo "$WILDFLY_VERSION" | sed 's/.[0-9]*.Final/.0.Final/')
+WILDFLY_GRAPHQL_BASE_VERSION=$(echo "$WILDFLY_VERSION" | sed 's/.[0-9]*.[0-9]*.Final/.0.0.Final/')
 
 URL=https://github.com/wildfly-extras/wildfly-graphql-feature-pack/releases/download/${WILDFLY_GRAPHQL_VERSION}/wildfly-${WILDFLY_GRAPHQL_BASE_VERSION}-mp-graphql-${WILDFLY_GRAPHQL_VERSION}-overlay.zip
 
